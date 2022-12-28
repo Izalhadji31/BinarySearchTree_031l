7b77ff2b0e42c143cbc6059fd6807a4e272cffbd
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-12-28 17:14:20 +0700

    Menambahkan class program

--- BinarySearchTree_031/Program.cs
+++ BinarySearchTree_031/Program.cs
@@ -8,6 +8,22 @@ namespace BinarySearchTree_031
 {
     class Node
     {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        // Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+
+
+    class program
+    {
 
     }
 }
